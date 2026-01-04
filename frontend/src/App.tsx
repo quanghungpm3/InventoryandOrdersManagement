e13ac63c3ccfa9e4959d731fd29aaf3cdbbf0b5a
Quang Hưng <134192527+quanghungpm3@users.noreply.github.com>
2026-01-04 12:36:56 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import SignInPage from "./pages/SignInPage";
-import WebAppPage from "./pages/WebAppPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInPage from "@/pages/SignInPage";
+import WebAppPage from "@/pages/WebAppPage";
+import DashboardPage from "@/pages/DashboardPage";
+import OrderPage from "@/pages/OrderPage";
 import { Toaster } from "sonner";
-import SignUpPage from "./pages/SignUpPage";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
+import SignUpPage from "@/pages/SignUpPage";
+import ProtectedRoute from "@/components/auth/ProtectedRoute";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
@@ -22,12 +25,26 @@ function App() {
           />
 
           {/* protectect routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route
-              path="/"
-              element={<WebAppPage />}
-            />
-          </Route>
+            <Route element={<ProtectedRoute />}>
+              <Route
+                path="/"
+                element={<WebAppPage />}
+              />
+              <Route
+                path="/"
+                element={<DashboardPage />}
+              />
+              <Route
+                path="/products"
+                element={<OrderPage />}
+              />
+              <Route
+                path="/orders"
+                element={<ProductPage />}
+              />
+            </Route>
+
+            
           
         </Routes>
       </BrowserRouter>
